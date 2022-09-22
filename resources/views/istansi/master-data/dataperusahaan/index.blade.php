9ac4737abbaf094920a4776bde67749b909fa05d
umiambarsari <101268508+SYARIFAHPUTRI@users.noreply.github.com>
2022-09-22 09:39:44 +0700

    up

--- resources/views/istansi/master-data/dataperusahaan/index.blade.php
+++ resources/views/istansi/master-data/dataperusahaan/index.blade.php
@@ -1,7 +1,7 @@
 <x-module.istansi>
 
-        <h5 class="m-0 font-weight-bold text-dark" style="text-align:center; font-size: 25px"> Data Perusahaan
-        </h5>
+    <h5 class="m-0 font-weight-bold text-dark" style="text-align:center; font-size: 25px"> Data Perusahaan
+    </h5>
 
     <div class="card">
         <div class="card-header">
@@ -11,7 +11,7 @@
         </div>
         <div class="card-body">
             <table id="example1" class="table table-bordered table-striped">
-              <thead class="bg-dark">
+                <thead class="bg-dark">
                     <th style="width : 1%">No</th>
                     <th width="100px">Aksi</th>
                     <th>Nama</th>
@@ -28,7 +28,7 @@
                             <td>
                                 <div class="btn-group">
                                     <x-template.button.info-button url="istansi/master-data/dataperusahaan"
-                                        id="{{ $dataperusahaan->id }}"  title="lihat detail"/>
+                                        id="{{ $dataperusahaan->id }}" title="lihat detail" />
                                     {{-- <x-template.button.edit-button url="istansi/master-data/dataperusahaan"
                                         id="{{ $dataperusahaan->id }}"/>
                                     <x-template.button.delete-button url="istansi/master-data/dataperusahaan"
@@ -36,8 +36,8 @@
                                 </div>
                             </td>
                             <td>{{ $dataperusahaan->nama_perusahaan }}</td>
-                            <td>{{ $dataperusahaan->alamat}}</td>
-                            <td>{{ 0 }}</td>
+                            <td>{{ $dataperusahaan->alamat }}</td>
+                            <td>{{ 2 }}</td>
                         </tr>
                     @endforeach
                 </tbody>
